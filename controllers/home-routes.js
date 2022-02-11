fa012e30bd87d0f54df12094e99a7d497c7436cc
sm3131 <sm2683@nau.edu>
2022-02-11 03:25:53 -0700

    added community workout page and changed home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,52 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Workout, User, Comment, Vote } = require('../models');
 
+// router.get('/', (req, res) => {
+//     console.log(req.session);
+
+//     Workout.findAll({
+//         attributes: [
+//             'id',
+//             'workout_title',
+//             'workout_description',
+//             'workout_length',
+//             'created_at',
+//             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE workout.id = vote.workout_id)'), 'vote_count']
+//         ],
+//         include: [
+//             {
+//                 model: Comment,
+//                 attributes: ['id', 'comment_text', 'workout_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     })
+//         .then(dbWorkoutData => {
+//             // pass a single workout object into the homepage template
+//             const workouts = dbWorkoutData.map(workout => workout.get({ plain: true }));
+//             res.render('homepage', {
+//                 workouts
+//                 //loggedIn: req.session.loggedIn
+//             });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
 router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+router.get('/community', (req, res) => {
     console.log(req.session);
 
     Workout.findAll({
@@ -32,7 +77,7 @@ router.get('/', (req, res) => {
         .then(dbWorkoutData => {
             // pass a single workout object into the homepage template
             const workouts = dbWorkoutData.map(workout => workout.get({ plain: true }));
-            res.render('homepage', {
+            res.render('community-workouts', {
                 workouts
                 //loggedIn: req.session.loggedIn
             });
@@ -42,6 +87,7 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
 // router.get('/login', (req, res) => {
 //     if (req.session.loggedIn) {
 //         res.redirect('/');
