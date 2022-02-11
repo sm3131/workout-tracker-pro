561837febab3979c6c21b54091304d36ca127495
sm3131 <sm2683@nau.edu>
2022-02-10 18:59:48 -0700

    added route and edited handlebar page to display edit-workout page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -43,4 +43,52 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/edit/:id', (req, res) => {
+    Workout.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'workout_title',
+            'workout_description',
+            'workout_length',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE workout.id = vote.workout_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'workout_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbWorkoutData => {
+            if (!dbWorkoutData) {
+                res.status(404).json({ message: 'No workout found with this id' });
+                return;
+            }
+            // serialize the data
+            const workout = dbWorkoutData.get({ plain: true });
+            console.log(workout);
+            // pass data to template
+            res.render('edit-workout', {
+                workout
+                // loggedIn: true
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
 module.exports = router;
