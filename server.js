adf389a661653f20ffc098813e327e269eebb1fb
sm3131 <sm2683@nau.edu>
2022-02-11 04:14:03 -0700

    added images to homepage

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static("images"));
 
 const hbs = exphbs.create({ helpers });
 
